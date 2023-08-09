16a78604d52327691ca51984a93c7bb020d0f99b
tanimaroy2012 <45754357+tanimaroy2012@users.noreply.github.com>
2023-08-09 11:27:31 +0100

    updated workflow

--- test/transform.js
+++ test/transform.js
@@ -1,11 +1,24 @@
-const collection = require('./PactflowProductsAPI.postman_collection.json');
-const { transpile } = require('postman2openapi');
-const fs = require('fs')
+// const collection = require('./PactflowProductsAPI.postman_collection.json');
+// const { transpile } = require('postman2openapi');
+// const fs = require('fs')
 
-const postman = JSON.stringify(collection);
-const openapi = transpile(postman, 'yaml');
-const filePath = __dirname + "/../oas/swagger.yml"
+// const postman = JSON.stringify(collection);
+// const openapi = transpile(postman, 'yaml');
+// const filePath = __dirname + "/../oas/openapi.yml"
 
-fs.writeFileSync(filePath, openapi);
+// fs.writeFileSync(filePath, openapi);
 
-console.log('Postman collection successfully converted to OAS and stored at the following location',filePath)
+// console.log('Postman collection successfully converted to OAS and stored at the following location',filePath)
+
+const postmanToOpenApi = require('postman-to-openapi')
+
+const postmanCollection = './test/PactflowProductsAPI.postman_collection.json'
+const outputFile = __dirname + "/../oas/swagger.yml"
+
+postmanToOpenApi(postmanCollection, outputFile, { defaultTag: 'General' })
+    .then(result => {
+        console.log(`OpenAPI specs: ${result}`)
+    })
+    .catch(err => {
+        console.log(err)
+    });
